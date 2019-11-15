a795a81ddb3dc11b17fce157375e7693579de2f1
Abzal Bacchus <bacchusabzal1@gmail.com>
2019-11-15 02:17:40 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,7 +48,8 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
-    result = []
+  
+  result = []
   index = 0
 
   while index < movies_collection.length do
@@ -56,7 +57,6 @@ def movies_with_director_key(name, movies_collection)
     result << movie_with_director_name(name, movie_data)
     index += 1
   end
-
   result
 end
 
@@ -73,7 +73,8 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-    result = {}
+    
+  result = {}
   index = 0
 
   while index < collection.length do
@@ -89,7 +90,7 @@ def gross_per_studio(collection)
 
   result
 end
-end
+
 
 def movies_with_directors_set(source)
   # GOAL: For each director, find their :movies Array and stick it in a new Array
@@ -102,7 +103,8 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-    index = 0
+  
+  index = 0
   a_o_a_movies_by_dir = []
 
   while index < source.length do
